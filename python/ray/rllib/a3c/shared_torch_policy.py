ccd9a51ed3c1651ea439574534dcdd1209c0d964
Alok Singh <alokbeniwal@gmail.com>
2018-05-06 10:51:06 +0000

    WIP

--- python/ray/rllib/a3c/shared_torch_policy.py
+++ python/ray/rllib/a3c/shared_torch_policy.py
@@ -33,8 +33,8 @@ class SharedTorchPolicy(TorchPolicy):
         with self.lock:
             ob = Variable(torch.from_numpy(ob).float().unsqueeze(0))
             logits, values = self._model(ob)
-            samples = self._model.probs(logits).multinomial(num_samples=1).squeeze()
             values = values.squeeze()
+            samples = self._model.probs(logits).multinomial(num_samples=1).squeeze()
             return var_to_np(samples), {"vf_preds": var_to_np(values)}
 
     def compute_logits(self, ob, *args):
